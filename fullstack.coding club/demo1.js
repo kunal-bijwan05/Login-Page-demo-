b86ae11396de4b8b194007d277825af9d3738f25
Kunal Singh Bijwan <kunalbijwan02@gmail.com>
2025-10-06 18:15:22 +0530

    Add files via upload

---
+++ fullstack.coding club/demo1.js
@@ -0,0 +1,7 @@
+let num1 = "10"
+let num2 = "20.6"
+let num11 = Number(num1)
+let num21 = Number(num2)
+console.log(num11+num21)
+console.log(typeof sum)
+console.log(typeof sum)
---
+++ fullstack.coding club/first.js
@@ -0,0 +1,6 @@
+start = 0
+while(start < 5){
+    console.log(100)
+    start++
+}
+
